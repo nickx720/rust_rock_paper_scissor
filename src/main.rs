eef94e49c383c68147975ca5450fe131d3c5787d
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-05-02 17:40:56 -0400

    Initial Setup Of Cross Term

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,84 @@
-fn main() {
-    println!("Hello, world!");
+use chrono::{DateTime, Utc};
+use crossterm::{
+    event::{self, Event as CEvent},
+    terminal::enable_raw_mode,
+};
+use serde::{Deserialize, Serialize};
+use std::time::Duration;
+use std::{io, time::Instant};
+use std::{sync::mpsc, thread};
+use thiserror::Error;
+use tui::{self, backend::CrosstermBackend, Terminal};
+
+const DB_PATH: &str = "./data/db.json";
+
+#[derive(Serialize, Deserialize, Clone)]
+struct Pet {
+    id: usize,
+    name: String,
+    category: String,
+    age: usize,
+    created_at: DateTime<Utc>,
+}
+
+enum Event<I> {
+    Input(I),
+    Tick,
+}
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("error reading the DB file: {0}")]
+    ReadDBError(#[from] io::Error),
+    #[error("error parsing the DB file: {0}")]
+    ParseDBError(#[from] serde_json::Error),
+}
+
+#[derive(Copy, Clone, Debug)]
+enum MenuItem {
+    Home,
+    Pets,
+}
+
+impl From<MenuItem> for usize {
+    fn from(input: MenuItem) -> usize {
+        match input {
+            MenuItem::Home => 0,
+            MenuItem::Pets => 1,
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Cross term option
+    enable_raw_mode().expect("can run in raw mode");
+
+    // MPSC handler to communicate between input and rendering loop
+    let (tx, rx) = mpsc::channel();
+    let tick_rate = Duration::from_millis(200);
+    thread::spawn(move || {
+        let mut last_tick = Instant::now();
+        loop {
+            let timeout = tick_rate
+                .checked_sub(last_tick.elapsed())
+                .unwrap_or_else(|| Duration::from_secs(0));
+
+            if event::poll(timeout).expect("poll works") {
+                if let CEvent::Key(key) = event::read().expect("can read events") {
+                    tx.send(Event::Input(key)).expect("Can send events");
+                }
+            }
+            if last_tick.elapsed() >= tick_rate {
+                if let Ok(_) = tx.send(Event::Tick) {
+                    last_tick = Instant::now();
+                }
+            }
+        }
+    });
+
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    
+    todo!();
 }
